7252d1b81a82dd8e1c3beaf3dc57207185658de6
Luis Alcaras <luisalcarasr@gmail.com>
2022-07-23 12:36:15 -0500

    👷 LSP Configuration has been refactored.

---
+++ lua/settings/lsp/init.lua
@@ -0,0 +1,8 @@
+-- Language Servers
+require('settings.lsp.servers.typescript')
+require('settings.lsp.servers.eslint')
+require('settings.lsp.servers.python')
+require('settings.lsp.servers.lua')
+
+-- Completion Menu
+require('settings.lsp.menu')
---
+++ lua/settings/lsp/servers/common.lua
@@ -0,0 +1,3 @@
+return {
+  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+}
