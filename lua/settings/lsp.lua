0476bb734d3466c3b1f80401bd0eab0d475e6ce3
Luis Alcaras <luisalcarasr@gmail.com>
2022-07-23 01:10:18 -0500

    🔧 ESLint autofix on save has been enabled.

--- lua/settings/lsp.lua
+++ lua/settings/lsp.lua
@@ -92,6 +92,13 @@ cmp.setup.cmdline(':', {
   })
 })
 
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    vim.cmd('EslintFixAll')
+  end,
+  pattern = {"*.js", "*.jsx", "*.ts", "*.tsx"}
+})
+
 -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
