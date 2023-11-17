ee2fc8e43d90b64e96844bff8178a8259f123fce
Luis Alcaras <luis.alcaras@autodesk.com>
2023-11-16 18:19:03 -0600

    🦇 Dracula theme has been added.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,13 @@ return {
   -- Colorschemes
   { "ellisonleao/gruvbox.nvim" },
   { "navarasu/onedark.nvim" },
+  { "Mofiqul/dracula.nvim" },
 
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "dracula",
     },
   },
 }
