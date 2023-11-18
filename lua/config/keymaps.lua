e179be873b35c677cfea99152efd1a94bb0fb2ae
Luis Alcaras <luis.alcaras@autodesk.com>
2023-11-18 00:29:32 -0600

    ⌨️ Terminal keymaps have been changed.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,42 +13,10 @@ groups.register({
 
 -- Terminal
 
-key.set(
-  { "n", "v" },
-  "<c-t>",
-  '<cmd>exe v:count1 . "ToggleTerm"<cr>',
-  { desc = "Toggle terminal", silent = true, noremap = true }
-)
-key.set(
-  "i",
-  "<c-t>",
-  '<esc><cmd>exe v:count1 . "ToggleTerm"<cr>',
-  { desc = "Toggle terminal", silent = true, noremap = true }
-)
+key.set("n", "<leader>tt", "<cmd>TermSelect<cr>", { desc = "Select terminal", silent = true, noremap = true })
+key.set("n", "<leader>tr", "<cmd>ToggleTermSetName<cr>", { desc = "Rename terminal", silent = true, noremap = true })
+key.set("n", "<leader>to", "<cmd>ToggleTerm<cr>", { desc = "Open terminal", silent = true, noremap = true })
 key.set("t", "<c-t>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal", silent = true, noremap = true })
-key.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal", silent = true, noremap = true })
-key.set(
-  "n",
-  "<leader>tl",
-  "<cmd>ToggleTermSendCurrentLine<cr>",
-  { desc = "Send current line", silent = true, noremap = true }
-)
-key.set(
-  "v",
-  "<leader>tl",
-  "<cmd>ToggleTermSendVisualLines<cr>",
-  { desc = "Send visual lines", silent = true, noremap = true }
-)
-key.set(
-  "v",
-  "<leader>ts",
-  "<cmd>ToggleTermSendVisualSelection<cr>",
-  { desc = "Send visual selection", silent = true, noremap = true }
-)
-key.set("t", "<leader>tn", "<cmd>ToggleTermSetName<cr>", { desc = "Set name", silent = true, noremap = true })
-key.set(
-  "n",
-  "<leader>ta",
-  "<cmd>ToggleTermToggleAll<cr>",
-  { desc = "Toggle all terminals", silent = true, noremap = true }
-)
+key.set("n", "<c-t>", function()
+  vim.cmd(vim.v.count .. "ToggleTerm")
+end, { desc = "Toggle terminal", silent = true, noremap = true })
